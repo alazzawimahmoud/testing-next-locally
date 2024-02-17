71397b07841c60a7aa6d63c368fb28f1fc91da78
alazzawimahmoud <alazzawi.mahmoud@gmail.com>
2024-02-17 17:02:03 +0100

    Adding stuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
+"use client"
 import Image from "next/image";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    fetch('http://localhost:3000/api/hello').then(c => c.json()).then(console.log)
+  }, [])
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
